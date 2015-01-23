393958f9108b43a225ceaf6119e364144655745b
Sylvain Fischer <sylvain.fischer@epitech.eu>
2015-01-23 15:12:28 +0100

    First commit on SumoDrive

--- SumoDrive/src/main/java/com/drive/sumo/FXMLController.java
+++ SumoDrive/src/main/java/com/drive/sumo/FXMLController.java
@@ -8,30 +8,27 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javax.sound.midi.SysexMessage;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     private SumoTraciConnection stc = null;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
-        
-       
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
@@ -41,9 +38,10 @@ public class FXMLController implements Initializable {
             lanes.forEach((k, p) -> {
                 System.out.println("p :" + p);
             });
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 10000; i++) {
                 stc.nextSimStep();
-            
+            }
+
         } catch (UnknownHostException ex) {
             Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -51,5 +49,5 @@ public class FXMLController implements Initializable {
         } catch (InterruptedException ex) {
             Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }    
+    }
 }
